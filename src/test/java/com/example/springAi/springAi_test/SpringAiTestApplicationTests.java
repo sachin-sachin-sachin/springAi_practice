a890505ff7a4469dffe89aa1697ed1a5ba9d84c5
sachin-sachin-sachin <jk7876632518@gmail.com>
2025-11-14 23:04:17 +0530

    Initial Spring Boot project commit

---
+++ src/test/java/com/example/springAi/springAi_test/SpringAiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springAi.springAi_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
