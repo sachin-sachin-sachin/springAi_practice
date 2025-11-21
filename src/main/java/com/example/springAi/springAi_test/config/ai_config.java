d861ef78b3ff2349eb30f6ed0fdb8893facbe6b3
sachin-sachin-sachin <jk7876632518@gmail.com>
2025-11-21 23:33:47 +0530

    correction in config file for adviser information about token or metadata  of logging in console

--- src/main/java/com/example/springAi/springAi_test/config/ai_config.java
+++ src/main/java/com/example/springAi/springAi_test/config/ai_config.java
@@ -1,15 +1,21 @@
 package com.example.springAi.springAi_test.config;
 
+import java.util.List;
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
 import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.example.springAi.springAi_test.Advisers.TokenPrintAdviser;
 
 @Configuration
 public class ai_config {
 	   @Bean
 	    public ChatClient chatClient(ChatClient.Builder builder) {
 	        return builder
+	        		 .defaultAdvisors(new TokenPrintAdviser(), new SafeGuardAdvisor(List.of("games")))
 	                .defaultSystem("You are a helpful coding assistant. You are an expert in coding.")
 	                .defaultOptions(OllamaChatOptions.builder()
 	                        .model("deepseek-coder:latest")
